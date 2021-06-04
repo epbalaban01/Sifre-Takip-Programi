498c3a7970ce88dcca0621c016a755f6a39bb53f
epbalaban01 <epbalaban01@gmail.com>
2021-06-05 01:06:37 +0300

    Şifre Takip Programı

---
+++ SifreTakipUygulama/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace SifreTakipUygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(710, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Otomatik Şifre";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 115);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(710, 108);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Manuel Şifre";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(12, 229);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(710, 146);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sadece Büyük Harf",
+            "Sadece Küçük Harf",
+            "Sadece Rakam",
+            "Rakam ve Özel Karakterler",
+            "Harfler (B/K) ve Özel Karakterler",
+            "Tamamı Karışık"});
+            this.comboBox1.Location = new System.Drawing.Point(171, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(171, 55);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zorluk Seç:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre Kaç Karakter Olacak?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yazı ile üretmek \r\nisterseniz buraya yazın:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Koymak İstediğiniz İsim:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Oluşturulan Şifre:";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(364, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(364, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Oluştur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(364, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(187, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(204, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Benzer Harf / Sayı Dönüşümlerini Yap";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 46);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Ben 2011 yılının 7. ayında mezun oldum.";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(171, 86);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 33);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(171, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(561, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(32, 27);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(561, 31);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 27);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.DialogResult = System.Windows.Forms.DialogResult.Ignore;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(397, 14);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(23, 25);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "?";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 390);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Şifre Takip Uygulama";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ SifreTakipUygulama/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SifreTakipUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("Yazılan cümleyi oluşturan karakterler kullanılarak ve aralara özel karakterler serpiştirilereksadece size özel bir şifre üretir.","BİLGİ", MessageBoxButtons.OK , MessageBoxIcon.Question);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("Seçilen zorluğa göre ASCII kod tablosundan rastgele karakterler seçilerek, istenen uzunlukta şifre oluşturulur", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fileName = System.Windows.Forms.Application.StartupPath + "//Sifrelerim.txt";
+                string writeText = textBox3.Text + " = " + textBox2.Text;
+                FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Close();
+
+                File.AppendAllText(fileName, Environment.NewLine + writeText);
+                MessageBox.Show("Kaydedildi");
+                button3.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Kaydedilemedi" + Environment.NewLine + ex.Message);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = new DialogResult();
+            dialog = MessageBox.Show("a=@, S=5, G=6, g=9, l=1,  Z=2, B=8, O=0, T=7", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        int[] zorluk;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+            textBox2.Text = "";
+            int sifre;
+            string karakter = "";
+
+            //zorluk = new int[] { 33, 47 };// ASCII kod tablosundaki özel karakter kodları #1
+            //zorluk = new int[] { 48, 57 };// ASCII kod tablosundaki rakam karakter kodları
+            //zorluk = new int[] { 58, 64 };// ASCII kod tablosundaki özel karakter kodları #2
+            //zorluk = new int[] { 65, 90 };// ASCII kod tablosundaki Büyük harf karakter kodları
+            //zorluk = new int[] { 91, 96 };// ASCII kod tablosundaki özel karakter kodları #3
+            //zorluk = new int[] { 97, 122 };// ASCII kod tablosundaki Küçük harf karakter kodları
+
+
+            Random rastgele = new Random();
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                switch (comboBox1.SelectedIndex)
+                {
+                    //Sadece Büyük Harf
+                    case 0: zorluk = new int[] { 65, 90 }; break;
+                    //Sadece Küçük Harf
+                    case 1: zorluk = new int[] { 97, 122 }; break;
+                    //Sadece Rakam
+                    case 2: zorluk = new int[] { 48, 57 }; break;
+                    //Rakam ve Özel Karakterler
+                    case 3: zorluk = new int[] { 33, 64 }; break;
+                    //Harfler Özel Karakterler
+                    case 4: zorluk = new int[] { 58, 122 }; break;
+                    //Tamamı Karışık
+                    case 5: zorluk = new int[] { 33, 122 }; break;
+                }
+
+                for (int i = 0; i < numericUpDown1.Value; i++)
+                {
+                    sifre = rastgele.Next(zorluk[0], zorluk[1]); // ASCII tablosundan rastgele bir karakter seçiyoruz.
+                    karakter += Convert.ToChar(sifre).ToString(); // Rastgele seçilmiş olan sayıyı harf ve özel karakterlere çeviriyoruz.
+                }
+                if (checkBox1.CheckState == CheckState.Checked)
+                    textBox2.Text = SayiHarfDonusum(karakter);
+                else
+                    textBox2.Text = karakter;
+            }
+            else
+            {
+                MessageBox.Show("Zorluk seçmelisiniz.");
+            }
+
+
+        }
+
+        string SayiHarfDonusum(string sifre)
+        {
+            return sifre.Replace("a", "@").Replace("S", "5").Replace("G", "6").Replace("g", "9").Replace("l", "1").Replace("Z", "2")
+                .Replace("B", "8").Replace("O", "0").Replace("T", "7");
+        }
+
+        bool SayiMi(string text)
+        {
+            foreach (char chr in text)
+            {
+                if (!Char.IsNumber(chr)) return false;
+            }
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+            string cumle = textBox1.Text;
+            string[] parcalar = cumle.Split(' ');
+            string sifre = "", karakter = "";
+            int random_Secim = 0;
+            Random rastgele = new Random();
+            zorluk = new int[] { 33, 47 };// ASCII kod tablosundaki özel karakter kodları #1
+
+            for (int i = 0; i < parcalar.Count(); i++)
+            {
+                random_Secim = rastgele.Next(zorluk[0], zorluk[1]);
+                karakter = Convert.ToChar(random_Secim).ToString();
+
+                if (SayiMi(parcalar[i]))
+                {
+                    if (parcalar[i].Length > 3)
+                        sifre += parcalar[i].Substring(0, 2) + karakter + parcalar[i].Substring(2, 2);
+                }
+                else
+                {
+                    if (rastgele.Next(0, 2) == 0)
+                    {
+                        sifre += parcalar[i].Substring(0, 1).ToLower();
+                        if (rastgele.Next(0, 2) == 1)
+                            sifre += karakter;
+                    }
+                    else
+                    {
+                        sifre += parcalar[i].Substring(0, 1).ToUpper();
+                        if (rastgele.Next(0, 2) == 0)
+                            sifre += karakter;
+                    }
+                }
+            }
+            sifre += karakter;
+
+            if (checkBox1.CheckState == CheckState.Checked)
+                textBox2.Text = SayiHarfDonusum(sifre);
+            else
+                textBox2.Text = sifre;
+        }
+    }
+}
